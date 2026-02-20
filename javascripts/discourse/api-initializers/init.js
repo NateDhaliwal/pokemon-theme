59b7f2835f8f68536a3b46af4137a87b2b6a463e
NateDhaliwal <nathaniel.shaan@gmail.com>
2026-02-20 22:04:03 +0800

    Update init.js

--- javascripts/discourse/api-initializers/init.js
+++ javascripts/discourse/api-initializers/init.js
@@ -1,9 +1,24 @@
+import { htmlSafe } from "@ember/template";
 import { apiInitializer } from "discourse/lib/api";
+import { iconHTML } from "discourse/lib/icon-library";
+import User from "discourse/models/user";
 
 function getParamUsername(currentRoute) {
   if (currentRoute.params.username) return currentRoute.params.username;
 
   return getParamUsername(currentRoute.parent);
+}
+
+function getGroupSettingData(currentUser) {
+  for (const groupSetting of settings.group_icon_data) {
+    for (const group of currentUser.groups) {
+      if (group.id === groupSetting.group) {
+        return groupSetting;
+      }
+    }
+  }
+
+  return null;
 }
 
 export default apiInitializer((api) => {
@@ -12,8 +27,34 @@ export default apiInitializer((api) => {
   api.onPageChange((url, title) => {
     const currentRoute = router.currentRoute;
     const currentUser = api.container.lookup('service:currentUser');
-    console.log(currentUser);
-    console.log(currentRoute);
-    console.log(getParamUsername(currentRoute));
+
+    let userProfileUsername;
+    try {
+      userProfileUsername = getParamsUsername(currentRoute);
+    } catch (TypeError) {
+      userProfileUsername = null;
+    }
+
+    if (userProfileUsername !== null) {
+      const groupData = getGroupSettingData(User.findByUsername(userProfileUsername));
+      document.getElementsByClassName(
+        "user-profile-names__primary"
+      )[0]
+        .children[0]
+        .replaceWith(
+          htmlSafe(
+            iconHTML(groupData.icon, {
+              // eslint-disable-next-line no-undef
+              label: groupData.iconLabel
+            }),
+          )
+        );
+    }
+
+    const postAuthors = document.getElementsByClassName("topic-meta-data");
+
+    if (postAuthors.length === 0 && currentRoute.parent.name === "topic") {
+      
+    }
   });
 });
