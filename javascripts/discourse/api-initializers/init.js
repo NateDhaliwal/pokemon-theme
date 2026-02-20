b48aabbdb42982614f99255accb9d30a0a79796f
NateDhaliwal <nathaniel.shaan@gmail.com>
2026-02-20 08:43:32 +0800

    Update init.js

--- javascripts/discourse/api-initializers/init.js
+++ javascripts/discourse/api-initializers/init.js
@@ -4,8 +4,8 @@ export default apiInitializer((api) => {
   const router = api.container.lookup('service:router');
   
   api.onPageChange((url, title) => {
-    const currentRoute = router.currentRoute.name);
+    const currentRoute = router.currentRoute.name;
     const currentUser = api.container.lookup('service:currentUser');
     console.log(currentUser);
-  })
+  });
 });
