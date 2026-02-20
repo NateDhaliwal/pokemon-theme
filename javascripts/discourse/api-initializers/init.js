79602ddddfb3abe8761f8ab69c1ce6e7a905c7d8
NateDhaliwal <nathaniel.shaan@gmail.com>
2026-02-20 08:47:01 +0800

    Use recursion to get username

--- javascripts/discourse/api-initializers/init.js
+++ javascripts/discourse/api-initializers/init.js
@@ -1,12 +1,18 @@
 import { apiInitializer } from "discourse/lib/api";
 
+function getParamUsername(currentRoute) {
+  if (currentRoute.params.username) return currentRoute.params.username;
+
+  return getParamUsername(currentRoute.parent);
+}
+
 export default apiInitializer((api) => {
   const router = api.container.lookup('service:router');
   
   api.onPageChange((url, title) => {
-    const currentRoute = router.currentRoute.name;
+    const currentRoute = router.currentRoute;
     const currentUser = api.container.lookup('service:currentUser');
     console.log(currentUser);
-    console.log(router.currentRoute);
+    console.log(getParamUsername(currentRoute);
   });
 });
