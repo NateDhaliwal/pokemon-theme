d5ee961440983f688aba7aea27d85b7e9248c578
NateDhaliwal <nathaniel.shaan@gmail.com>
2026-02-20 08:39:55 +0800

    Update init.js

--- javascripts/discourse/api-initializers/init.js
+++ javascripts/discourse/api-initializers/init.js
@@ -1,7 +1,9 @@
 import { apiInitializer } from "discourse/lib/api";
 
 export default apiInitializer((api) => {
+  const router = api.container.lookup('service:router');
+  
   api.onPageChange((url, title) => {
-    console.log(url, title);
+    console.log(router.currentRoute.name);
   })
 });
